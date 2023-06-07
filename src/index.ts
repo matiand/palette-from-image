c4e1189c06f3a3bde24eec14b03359b3879e5f69
matiand <15248621+matiand@users.noreply.github.com>
2023-06-07 23:09:40 +0200

    Stop using a11y plugin in our colord instances

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-import { extend } from "colord";
-import a11y from "colord/plugins/a11y";
-
-extend([a11y]);
-
 export { paletteFromImage, type FromImageOptions } from "./paletteFromImage";
 export {
     paletteFromPixels,
